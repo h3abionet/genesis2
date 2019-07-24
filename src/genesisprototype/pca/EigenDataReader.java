8c918ce08054a493d37022a36595f5f5e5b67efb
Salmi Ayyoub <smile-up@live.fr>
2019-07-24 13:26:51 +0100

    Added eigen format reader for Plink and Eigenstrat files

--- src/genesisprototype/pca/EigenDataReader.java
+++ src/genesisprototype/pca/EigenDataReader.java
@@ -10,28 +10,75 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * project genesis2
- * Created by ayyoub on 2019-07-23.
+ * This class contains the methods needed to convert eigenstrat/Plink input into
+ * the PCASubject array
+ *
+ * @author A Salmi
  */
 public class EigenDataReader {
 
-    public EigenDataReader() {
+    private EigenDataReader() {
     }
 
+    /**
+     * Create a PCASubject list from file
+     *
+     * @param inputFile the eigenstrat/Plink input file
+     * @return PCASubject list
+     * @throws EigenDataException indicates a buggy file
+     */
     public static List<PCASubject> createDataFromEigenInput(String inputFile) throws EigenDataException {
         if (!EigenDataReader.checkEigenFile(inputFile))
             return null;
 
-        return extractDataFromFile(inputFile);
+        if (inputFile.endsWith(".eigenvec"))
+            return extractDataFromPlinkFile(inputFile);
+        else if (inputFile.endsWith(".evec"))
+            return extractDataFromEigenstratFile(inputFile);
+
+        return null;
     }
 
-    private static List<PCASubject> extractDataFromFile(String inputFile) {
+
+    /**
+     * Parse PCASubject objects from Eigenstrat file
+     *
+     * @param inputFile the eigenstrat input file
+     * @return PCASubject list
+     */
+    private static List<PCASubject> extractDataFromEigenstratFile(String inputFile) {
+        List<PCASubject> pcaList = new ArrayList<>(100);
+        try {
+            Files.lines(Paths.get(inputFile))
+                    .map(l -> l.trim().replaceAll(" +", " "))
+                    .forEach(l -> {
+                        if (l.startsWith("#"))
+                            return;
+                        String[] split = l.split("\\s+");
+                        //TODO check if lines have different numbers of components
+                        PCASubject object = new PCASubject(extractName(split), conv(split, 1));
+                        pcaList.add(object);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return pcaList;
+    }
+
+    /**
+     * Parse PCASubject objects from Plink file
+     *
+     * @param inputFile the eigenstrat input file
+     * @return PCASubject list
+     */
+    private static List<PCASubject> extractDataFromPlinkFile(String inputFile) {
         List<PCASubject> pcaList = new ArrayList<>(100);
         try {
             Files.lines(Paths.get(inputFile)).forEach(l -> {
                 String[] split = l.trim().split("\\s+");
                 //TODO check if lines have different numbers of components
-                PCASubject object = new PCASubject(extractName(split), conv(split));
+                PCASubject object = new PCASubject(extractName(split), conv(split, 2));
                 pcaList.add(object);
             });
         } catch (IOException e) {
@@ -41,6 +88,13 @@ public class EigenDataReader {
         return pcaList;
     }
 
+    /**
+     * Extract the name from a line of the plin/eigenstrat input.
+     * plink line format: "firstname optionalsecondname col1 col2 ... "
+     * eigen line format: "firstname:optionalsecondname col1 col2 ... "
+     * @param input the (split) input line
+     * @return the name (first and second name seperated by a space if there are 2 names)
+     */
     private static String extractName(String[] input) {
         String[] nameArr = input[0].split(":");
         String name;
@@ -52,15 +106,30 @@ public class EigenDataReader {
         return name;
     }
 
-    private static double[] conv(String[] split) throws NumberFormatException {
-        double[] result = new double[split.length - 2];
-        for (int i = 2; i < split.length - 2; i++) {
-            result[i - 2] = Double.parseDouble(split[i].trim());
+    /**
+     * Converts 1 line of eigenstrat data to an array of doubles that represent
+     * the values for each PCA axis
+     *
+     * @param line   the (split) input line
+     * @param offset 1 for eigenstrat and 2 for plink
+     * @return the array of doubles
+     * @throws NumberFormatException
+     */
+    private static double[] conv(String[] line, int offset) throws NumberFormatException {
+        double[] result = new double[line.length - 2];
+        for (int i = offset; i < line.length - offset; i++) {
+            result[i - offset] = Double.parseDouble(line[i].trim());
         }
         return result;
-
     }
 
+
+    /**
+     * Checks input file for errors
+     * @param inputFile input file
+     * @return true if file is OK, otherwise throws an exception
+     * @throws EigenDataException
+     */
     private static boolean checkEigenFile(String inputFile) throws EigenDataException {
         // check if file exists
         if (!Files.exists(Paths.get(inputFile)))
